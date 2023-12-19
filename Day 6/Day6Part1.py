dfaef8b43c1bd2e9b8d87225b3e83d70795eef29
stuja16 <fastglove+github@gmail.com>
2023-12-19 15:00:09 -0600

    Working Solutions for Day 6

---
+++ Day 6/Day6Part1.py
@@ -0,0 +1,56 @@
+# https://adventofcode.com/2023/day/6
+
+import re
+
+def getWinProduct(fileName):
+    times = []
+    records = []
+    wins = 0
+
+    # Read input from text file
+    file = open(fileName,"r")
+    timeText = file.readline()
+    distText = file.readline()
+    file.close()
+
+    # Search for and log race times and distance records
+    pattern = re.compile(r'[\d]+')
+    matches = pattern.finditer(timeText)
+    for match in matches:
+        times.append(int(match.group()))
+    matches = pattern.finditer(distText)
+    for match in matches:
+        records.append(int(match.group()))
+    
+    # Check each possible length of time for holding down the button
+    for i in range(len(times)):
+        current = getWinNumbers(times[i],records[i])
+        if wins == 0:
+            wins = current
+        else:
+            wins *= current
+    
+    # Find product of winning options for each race
+    print(wins)
+
+# Input (time): Int - total available time to complete the race
+# Input (record): Int - previous distance record
+# Output: Int - # of distinct time lengths you can hold the button and still win
+def getWinNumbers(time, record):
+    max = int(time/2) + 1   # Resulting vector is reflexive, so we can halve the cases to check
+    wins = 0
+    for i in range(1,max):
+        if getRaceResults(i,time) > record:
+            wins += 1
+    if time % 2 == 0:
+        return wins*2 - 1
+    return wins*2
+
+# Input (held): Int - racetime spent holding the button down
+# Input (total): Int - total available time to complete the race
+# Output: Int - total distance travelled during the racetime
+def getRaceResults(held, total):
+    return (total-held)*held
+
+#getWinProduct("Day 6\Day6_UnitTest.txt")
+getWinProduct("Day 6\Day6_Input.txt")
