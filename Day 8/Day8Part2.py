446e65e0e1fb39e641cb667166f086a70056714e
stuja16 <fastglove+github@gmail.com>
2023-12-21 00:15:37 -0600

    Working & Improved Solutions for Day 8

--- Day 8/Day8Part2.py
+++ Day 8/Day8Part2.py
@@ -3,11 +3,11 @@
 # Solution improved with help from the solutions subreddit:
 #       https://www.reddit.com/r/adventofcode/comments/18df7px/comment/kcguu6a/
 
-import re
+import math, re
 
 def solve(fileName):
     global guide, text
-    steps, loc = (0, "AAA")
+    starts, locs = ("",[])
 
     # Read input from text file
     file = open(fileName,"r")
@@ -17,19 +17,25 @@ def solve(fileName):
     del (text[1])   # Remove blank line
     guide = text.pop(0)
     text = [line.split(", ") for line in text]
-
-    print(travel(loc))
+    pattern = re.compile(r'\w\wA')
+    for dir in text:
+        starts += dir[0] + " "
+    matches = pattern.finditer(starts)
+    for match in matches:
+        locs.append(match.group())
     
+    print(math.lcm(*map(travel, locs)))
+
 def travel(loc, i=0):
     while not loc.endswith("Z"):
         dir = guide[i % len(guide)]
         for set in text:
             s,*next = set
             if loc == s:
-                loc = next[dir=="R"]
+                loc = next[dir=="R"]    # Using a boolean to index a 2-element array
+                break
         i += 1
     return i
 
-#solve("Day 8\Day8_UnitTest1.txt")
-#solve("Day 8\Day8_UnitTest2.txt")
+#solve("Day 8\Day8_UnitTest3.txt")
 solve("Day 8\Day8_Input.txt")
