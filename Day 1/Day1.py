3b36d3a125a66f545d26e211178e5aaf7b50847e
stuja16 <fastglove+github@gmail.com>
2023-12-24 00:42:39 -0600

    String.replace(), more pythonic, and ability to run both parts

--- Day 1/Day1.py
+++ Day 1/Day1.py
@@ -1,44 +1,18 @@
 # https://adventofcode.com/2023/day/1
 
-digitTexts = {("zero", "0"), ("one", "1"), ("two", "2"), ("three", "3"), ("four", "4"), ("five", "5"), ("six", "6"), ("seven", "7"), ("eight", "8"), ("nine", "9")}
+import re
 
-# Solves Day 1 by opening/reading file and calling the needed functions to find the sum of all callibration values
-def getTotal():
-    file = open("Day 1\Day1_Input.txt","r")
-    sum = 0;    # Variable to store the sum (target value)
-    for each in file:
-        cv = getCallibrationValue(each.lower())
-        sum += cv
-    print("Sum of all Callibration Values:", sum)   # Print sum to the console
-    file.close()
+# Sums calibration values for each line
+def solve(p1 = True):   # If running part 2, set p1 = False
+    lines = [textToDigit(l) if not p1 else l for l in open("Day 1\Day1_Input.txt","r").read().strip().split("\n")]
+    print("Sum of all Callibration Values:", sum([int(n[0] + n[-1]) for n in ["".join(re.findall("\d+", l)) for l in lines]]))
 
-# Get the calibration value for a single token/string
-def getCallibrationValue(token):
-    forward = textToDigit(token)
-    backward = forward[::-1]
-    return int(getLeadingDigit(forward) + getLeadingDigit(backward))
-
-# Runs through the input one way to add digits before the last char in written numbers
+# Replaces all written numbers with numeric digits, preserving first and last character for overlapping numbers
 def textToDigit(text):
-    modified = True    # Keeps track of whether a numeric digit has been added this round
-    while modified:
-        modified = False
-        # Look for each number
-        for num in digitTexts:
-            if num[0] in text:
-                i = text.find(num[0])
-                text = text[:i + len(num[0]) - 1] + num[1] + text[i + len(num[0]) - 1:]
-                modified = True
+    digitTexts = {"one": "o1e", "two": "t2o", "three": "t3e", "four": "f4r", "five": "f5e", "six": "s6x", "seven": "s7n", "eight": "e8t", "nine": "n9e"}
+    for word,num in digitTexts.items():
+        text = text.replace(word,num)
     return text
 
-# Function to analyze the string
-def getLeadingDigit(text):
-    # Iterate through the input string
-    for c in text:
-        # Test the character against all possible digits
-        for x in range(10):
-            if ord(c) - 48 == x:
-                # Return the first digit in the string as a char datatype
-                return c
-
-getTotal()
+solve()         # part 1
+solve(False)    # part 2
